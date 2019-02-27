16ef05aff2f3fa69fd2717271e3b9fca32968b6e
mgsong <mgsong@jjsoft.kr>
2019-02-27 14:18:35 +0900

    불필요 소스 주석처리

--- src/app.js
+++ src/app.js
@@ -8,20 +8,20 @@ var options = {
     cert: fs.readFileSync('cert.pem')
 };
 
-var passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy;
+// var passport = require('passport')
+//   , FacebookStrategy = require('passport-facebook').Strategy;
 
-passport.use(new FacebookStrategy({
-    clientID: "595064647565644",
-    clientSecret: "54ea5eae609f169faec50003b60422ed",
-    callbackURL: "https://localhost/"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log("user login")
-    console.log(`accessToken = ${accessToken} , refreshToken = ${refreshToken}`)
-    done(null, user)
-  }
-));
+// passport.use(new FacebookStrategy({
+//     clientID: "595064647565644",
+//     clientSecret: "54ea5eae609f169faec50003b60422ed",
+//     callbackURL: "https://localhost/"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     console.log("user login")
+//     console.log(`accessToken = ${accessToken} , refreshToken = ${refreshToken}`)
+//     done(null, user)
+//   }
+// ));
 
 
 
@@ -44,26 +44,26 @@ https.createServer(options, app).listen(port2, function(){
 });
 
 
-app.get('/login', function (req, res){  
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h3>Login</h3>');
-    res.write('<form method="POST" action="/login">');
-    res.write('<label name="userId">UserId : </label>')
-    res.write('<input type="text" name="userId"><br/>');
-    res.write('<label name="password">Password : </label>')
-    res.write('<input type="password" name="password"><br/>');
-    res.write('<input type="submit" name="login" value="Login">');
-    res.write('</form>');
-    res.end();
-})
+// app.get('/login', function (req, res){  
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('<h3>Login</h3>');
+//     res.write('<form method="POST" action="/login">');
+//     res.write('<label name="userId">UserId : </label>')
+//     res.write('<input type="text" name="userId"><br/>');
+//     res.write('<label name="password">Password : </label>')
+//     res.write('<input type="password" name="password"><br/>');
+//     res.write('<input type="submit" name="login" value="Login">');
+//     res.write('</form>');
+//     res.end();
+// })
 
-app.post('/login', function (req, res){  
-    var userId = req.param("userId");
-    var password = req.param("password")
+// app.post('/login', function (req, res){  
+//     var userId = req.param("userId");
+//     var password = req.param("password")
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Thank you, '+userId+', you are now logged in.');
-    res.write(`your password : ${password} `);
-    res.write('<p><a href="/"> back home</a>');
-    res.end();
-});
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('Thank you, '+userId+', you are now logged in.');
+//     res.write(`your password : ${password} `);
+//     res.write('<p><a href="/"> back home</a>');
+//     res.end();
+// });
